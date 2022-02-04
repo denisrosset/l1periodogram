b090e36775d790f4ead7c3d6ff37ab6b4c3d84f1
Denis Rosset <physics@denisrosset.com>
2022-02-04 17:57:03 +0100

    New version with notebooks at the root folder.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,18 +1,31 @@
-# -*- coding: utf-8 -*-
+import warnings
 
 from pkg_resources import DistributionNotFound, get_distribution
+import sys
+from pathlib import Path
 
 try:
-    __version__ = get_distribution("emcee").version
+    import l1periodogram
+except ImportError as error:
+    package_path = Path(__file__).parent.parent.parent.resolve()
+    sys.path.append(str(package_path))
+    try:
+        import l1periodogram
+    except ImportError as error2:
+        warnings.warn('Could not import the l1periodogram package')
+
+try:
+    __version__ = get_distribution("l1periodogram").version
 except DistributionNotFound:
     __version__ = "unknown version"
 
 
 # General stuff
 extensions = [
-    'sphinxcontrib.bibtex',
+    "sphinxcontrib.bibtex",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
     "sphinx_autodoc_typehints",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
@@ -24,6 +37,8 @@ master_doc = "index"
 
 # myst_nb
 myst_enable_extensions = ["dollarmath", "colon_fence"]
+jupyter_execute_notebooks = "force"
+execution_timeout = -1
 
 # sphinxcontrib.bibtex
 bibtex_bibfiles = ['refs.bib']
@@ -32,7 +47,7 @@ project = "l1 periodogram"
 copyright = "2020-2022, Nathan C. Hara, Denis Rosset, Alessandro R. Mari & contributors"
 version = __version__
 release = __version__
-exclude_patterns = ["_build"]
+exclude_patterns = ["_build", "notebooks/build", "notebooks/data", "notebooks/docs", "notebooks/l1periodogram", "notebooks/*.md", "notebooks/*.rst"]
 
 # HTML theme
 html_theme = "sphinx_book_theme"
@@ -51,7 +66,5 @@ html_theme_options = {
     "use_repository_button": True,
     "use_download_button": True,
 }
-jupyter_execute_notebooks = "off"
-execution_timeout = -1
 html_extra_path = ["robots.txt"]
 
